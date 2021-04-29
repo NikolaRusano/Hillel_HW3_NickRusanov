15e4dad49295bca7b60082792b7ae0234121d89b
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-04-29 21:41:30 +0300

    First Commit. Task1 created

---
+++ src/GeneralTasks/Task1.java
@@ -0,0 +1,18 @@
+package GeneralTasks;
+
+import java.util.Scanner;
+
+//1.Написать программу с названием “CheckPassFail”, которая выводит PASS если
+//переменная “mark” типа int больше или равно 50 или вывести “FAIL” в противном
+//случае. Программа всегда должна перед выходом печатать “DONE”
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input mark value:");
+        int mark = scanner.nextInt();
+        if(mark >= 50){
+
+        }
+    }
+}
